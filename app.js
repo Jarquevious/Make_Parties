70b6fe02dac84d15b28b82fd3c1c3feaad885f05
Jarquevious <jarquevious@gmail.com>
2020-06-06 15:56:07 -0700

    Users can see all mock events

--- app.js
+++ app.js
@@ -10,9 +10,23 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Tell our app to send the "hello world" message to our home page
+// Render the "home" layout for the main page and send the following msg
 app.get('/', (req, res) => {
     res.render('home', { msg: 'Handlebars are Cool!' });
-})
+  })
+
+// OUR MOCK ARRAY OF PROJECTS
+var events = [
+    { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://3milliondogs.com/blog-assets-two/2015/08/admin-ajax-7.jpeg" },
+    { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://3milliondogs.com/blog-assets-two/2015/08/admin-ajax-7.jpeg" },
+    { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://3milliondogs.com/blog-assets-two/2015/08/admin-ajax-7.jpeg " }
+  ]
+  
+// INDEX
+  app.get('/events', (req, res) => {
+    res.render('events-index', { events: events });
+  })
+
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
